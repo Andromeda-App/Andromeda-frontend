9cd1e32e09ac9bc00e3f97f06df04cc942dd789f
kpham000 <90490117+kpham000@users.noreply.github.com>
2021-12-03 14:29:37 -0800

    iss trackin

--- src/App.js
+++ src/App.js
@@ -4,11 +4,62 @@ import API from "./utils/api.js"
 import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
 import Navbar from './components/NavBar';
-import { BrowserRouter as BrowserRouter, Routes, Route, Link, Redirect } from "react-router-dom"
+import { BrowserRouter as BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home";
 import Preferences from "./pages/Preferences";
 import Profile from "./pages/Profile";
-import './App.css'
+// kp Map & axios
+import Map from "./components/IssCard/Map";
+// import axios from 'axios';
+
+// try 2 kp
+// function App() {
+//   return (
+//     <div className="App">
+//       <h3>ISS Tracker</h3>
+//       <Maps />
+//     </div>
+
+//   );
+// }
+
+// try 2 kp
+
+// kp TRY 1
+// function App() {
+//   const [loading, SetLoading] = useState(false)
+//   const [longitude, setLongitude] = useState(122.3321)
+//   const [latitude, setLatitude] = useState(47.6062)
+
+//   useEffect(() => {
+//     getLocation()
+
+//   }, [])
+
+//   const getLocation = async () => {
+//       try {
+//         SetLoading(true)
+//         const res = await axios.get('http://api.open-notify.org/iss-now.json')
+//         const { longitude, latitude } = await res.data.iss_position
+
+//         setLongitude(parseFloat(longitude))
+//         setLatitude(parseFloat(latitude))
+//         SetLoading(false)
+//       }
+//       catch (error) {
+//         throw new Error(error)
+//       }
+//   }
+
+//   return <div className='App'>
+//     {!loading ? (
+//       <IssCard center={{ lat: latitude, lng: longitude }} zoom={6} />
+//     ) : (
+//       <h1>Loading</h1>
+//     )}
+//   </div>
+// }
+// kp TRY 1
 
 function App() {
   // User Login
@@ -29,11 +80,11 @@ function App() {
     password: ""
   })
   // Navbar
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  )
+  // return (
+  //   <div className="App">
+  //     <Navbar />
+  //   </div>
+  // )
 
   useEffect(() => {
     const myToken = localStorage.getItem("token");
@@ -132,7 +183,16 @@ function App() {
           <button onClick={logMeOut}>Logout</button>
           <Link to="/">Home</Link>
           <Link to={`/profile/${userState.id}`}>Profile</Link>
+          <div className="App">
+      <h3>ISS Tracker</h3>
+      <Map />
+    </div>
+
+  );
+          <Map></Map>
         </div>
+
+        
       )}
       <div className="App">
         <Navbar />
@@ -141,6 +201,7 @@ function App() {
           <Route exact path='/preferences' element={<Preferences />} user={userState} token={token} />
           <Route exact path='/profile' element={<Profile />} user={userState} token={token} />
         </Routes>
+ 
       </div>
     </BrowserRouter>);
 }
